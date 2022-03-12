dcdd9b685ebf634be20a182c889864b29825e40a
Edwin Mongare <edwinmongare15@gmail.com>
2022-03-13 00:32:18 +0300

    fixed catch error in consumer

--- Consumer.js
+++ Consumer.js
@@ -1,7 +1,7 @@
 const amqp = require("amqplib/callback_api");
 const axios = require("axios");
 
-amqp.connect("amqp://localhost", function async(error0, connection) {
+amqp.connect("amqp://127.0.0.1", function async(error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -18,49 +18,45 @@ amqp.connect("amqp://localhost", function async(error0, connection) {
         " [*] Waiting for messages in %s. To exit press CTRL+C",
         queue
       );
-      channel
-        .consume(queue, function (msg) {
-          if (msg.length <= 0) {
-            console.log("no message in queue");
-          }
-          const message = JSON.parse(msg.content.toString());
+      channel.consume(queue, function (msg) {
+        if (msg.length <= 0) {
+          console.log("no message in queue");
+        }
+        const message = JSON.parse(msg.content.toString());
 
-          const messageTwo = {
-            "imei": "865067025030191",
-            "deviceRuntime": "0",
-            "batteryLevel": "200",
-            "liters": "0",
-            "aquiferLevel": "0",
-            "knocksCount": "0",
-            "staticWaterLevel": "0",
-            "totalDepth": "0",
-            "dateSent": new Date(),
-          };
+        const messageTwo = {
+          "imei": "865067025030191",
+          "deviceRuntime": "0",
+          "batteryLevel": "200",
+          "liters": "0",
+          "aquiferLevel": "0",
+          "knocksCount": "0",
+          "staticWaterLevel": "0",
+          "totalDepth": "0",
+          "dateSent": new Date(),
+        };
 
-          //**  post to http endpoint
-          axios
-            .post(
-              "https://gosoftcoreapi.azurewebsites.net/Borehole/SaveSensorTelemetry",
-              message,
-              {
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            )
-            .then(
-              (response) => {
-                console.log("tcp message", response.data);
-                console.log("responseData Axios", response.status);
+        //**  post to http endpoint
+        axios
+          .post(
+            "https://gosoftcoreapi.azurewebsites.net/Borehole/SaveSensorTelemetry",
+            message,
+            {
+              headers: {
+                "Content-Type": "application/json",
               },
-              (error) => {
-                console.log("errorData Axios", error.message);
-              }
-            );
-        })
-        .catch(function (error) {
-          console.log(error, "err");
-        });
+            }
+          )
+          .then(
+            (response) => {
+              console.log("tcp message", response.data);
+              console.log("responseData Axios", response.status);
+            },
+            (error) => {
+              console.log("errorData Axios", error.message);
+            }
+          );
+      });
     },
     {
       noAck: true,
